c012e5c5d81e6d98a7c77fd93d5aac8815b5c809
Egor <egor.rajewski@gmail.com>
2019-12-10 17:40:44 +0200

    i think its final

--- js/script.js
+++ js/script.js
@@ -28,3 +28,15 @@ $(function () {
 //     })(jQuery);
 
 // });
+
+
+$(document).ready(function () {
+    $("a[href^='#']").click(function () {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 1200);
+        return false;
+    });
+});
